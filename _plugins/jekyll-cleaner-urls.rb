2053b462d3819759bb81c2f373f8fab61ec8aac0
salafimanhaj <67724995+salafimanhaj@users.noreply.github.com>
2020-08-24 23:16:38 +0700

    Create jekyll-cleaner-urls.rb

---
+++ _plugins/jekyll-cleaner-urls.rb
@@ -0,0 +1,26 @@
+﻿
+# MIT Licence
+
+# _plugins/jekyll-cleaner-urls.rb
+
+module Jekyll
+
+  class Post
+
+    #https://github.com/jekyll/jekyll/blob/master/lib/jekyll/post.rb
+    def id
+      File.join(dir, slug).gsub(" ","-").gsub("%20","-").gsub("--","-")
+    end
+
+  end
+
+  #https://github.com/jekyll/jekyll/blob/master/lib/jekyll/url.rb
+  class URL
+
+    def self.escape_path(path)
+      URI.escape(path, /[^a-zA-Z\d\-._~!$&'()*+,;=:@\/]/).encode('utf-8').gsub("%20","-").gsub(" ","-").gsub("--","-")
+end
+
+  end
+
+end
