a935f118b3d3518aeefa0e33debbdc82e266f260
alsalafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2022-05-06 12:03:46 +0700

    Delete hijri.rb

--- _plugins/hijri.rb
+++
@@ -1,9 +0,0 @@
-module DateFilter
-  MONTHS = %w(Muharram Safar Rabi1 Rabi2 Juma1 Juma2 Rajab Shaban Ramadan Shawwal DhulQadah DhulHijjah)
-
-  def hijri_month(input)
-    MONTHS[input.strftime("%m").to_i - 1]
-  end
-end
-
-Liquid::Template.register_filter(DateFilter)
