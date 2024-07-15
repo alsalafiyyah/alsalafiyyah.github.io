8cb13eb48cec93c796077d420c168e8416a8be52
alsalafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2024-07-15 07:43:41 +0700

    Update infinite-jekyll.js

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -7,7 +7,7 @@ $(function() {
       loadNewPostsThreshold = 3000;
   
   // Load the JSON file containing all URLs
-  $.getJSON('/posts.json', function(data) {
+  $.getJSON('https://alsalafiyyah.github.io/posts.json', function(data) {
     postURLs = data["posts"];
     
     // If there aren't any more posts available to load than already visible, disable fetching
