6fdfdbea7f017a9b1dac137a64a2df83280849bd
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-09-13 06:34:50 +0000

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.md',
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+ ],
+  theme: {
+    extend: {},
+ },
+  plugins: [],
+}
